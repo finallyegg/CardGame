178d307193422a8a5b4a658b23cc1effc6f5ca13
Jake Murphy <jsm38@buffalo.edu>
2018-09-11 19:42:09 -0400

    added a few tests

--- src/code/Deck/CardTest.java
+++ src/code/Deck/CardTest.java
@@ -6,9 +6,17 @@ import org.junit.jupiter.api.Test;
 
 class CardTest {
 
+
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_card() {
+		Card c = new Card(Card.Ranks.TWO, Card.Suits.CLUBS);
+		assertEquals(c.value() == 2, true);
+		assertEquals(c.getRank() == Card.Ranks.TWO, true);
+		
+		c = new Card(Card.Ranks.ACE, Card.Suits.CLUBS);
+		assertEquals(c.value() == 14, true);
+		assertEquals(c.getSuit() == Card.Suits.CLUBS, true);
 	}
 
 }
--- src/code/Deck/DeckTest.java
+++ src/code/Deck/DeckTest.java
@@ -2,6 +2,8 @@ package code.Deck;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 class DeckTest {
@@ -21,10 +23,14 @@ class DeckTest {
 
 		d.reset();
 		d.shuffle();
+		ArrayList<Card> d2 = new ArrayList<Card>();
+		System.out.println("Should look random...");
 		for(int i = 0 ; i < 52 ; i++) {
 			c = d.deal();
 			System.out.println(c.getRank() + " " + c.getSuit());
+			d2.add(c);
 		}
+		assertEquals(d2.size() == 52, true);
 
 	}
 
