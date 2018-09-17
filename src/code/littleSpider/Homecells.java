9fc1909ea4498295581fb677a8848be5d23e7866
yingwei123 <yli283@buffalo.edu>
2018-09-17 14:23:32 -0400

    I messed up last commit and I just realized. Mb

--- src/code/littleSpider/Homecells.java
+++ src/code/littleSpider/Homecells.java
@@ -11,14 +11,28 @@ public class Homecells {
 	public Homecells(Card s) {
 		card = s;
 		cardStack.add(s);
+		
 	}
-	
+	//this gets the first card and checks to see if the suits match
 	public boolean checkSuit() {
+		
+		if(cardStack.get(0).getSuit() == card.getSuit() ) {
+			return true;
+		}
+		else {
+			return false;	
+		}
+		
+	}
+	//this checks to see if the first card is either 1 or -1 off of the original card. for ex if the index was King it would check if the new card's
+	//value is either a queen or a Ace and if it is it will return true
+	public boolean checkNumber() {
+		if(cardStack.get(0).value() + 1 == card.value() || cardStack.get(0).value() -1 == card.value()) {
+			return true;
+		}
+		else {
 		return false;
 	}
-	
-	public boolean checkNumber() {
-		return false;
 	}
 	
 	public boolean checkFinished() {
@@ -26,7 +40,8 @@ public class Homecells {
 	}
 	
 	public void removeCard() {
-		
+	
 	}
 
 }
+
--- src/code/littleSpider/Tableaus.java
+++ src/code/littleSpider/Tableaus.java
@@ -2,11 +2,14 @@ package code.littleSpider;
 import java.util.ArrayList;
 
 import code.Deck.Card;
-
-public class Tableaus {
+import code.Deck.Deck;
+//I extended the Deck class since I wanted to use the deal() method
+public class Tableaus extends Deck{
 	
 	private ArrayList<Card> cardStack;
-	
+	private Card a;
+
+	//everytime you deal a would be equal to that card delt
 	public Tableaus(Card a, Card b, Card c, Card d, Card e, Card f) {
 		cardStack.add(a);
 		cardStack.add(b);
@@ -14,17 +17,30 @@ public class Tableaus {
 		cardStack.add(d);
 		cardStack.add(e);
 		cardStack.add(f);	
+		a = deal();
 	}
-	
+	//this method checks the card numbers to see if the delt card is either greater than the Tableaus first card by one or less than the Tableaus first card 
+	//by one. If it is then it will take that card a and put it into the Tableaus and become the first card.(I don't think we need a add card or remove
+	//card since a = deal() which returns a card which means it removes a card from a deck and returns a card and this method checks that card and adds
+	//it into the Tableaus. it would deal a new card and Card a would be that new value
 	public boolean checkNumber() {
+		if(cardStack.get(0).value() - 1 == a.value()  || cardStack.get(0).value() + 1 == a.value()) {
+			cardStack.add(a);
+			a = deal();
+			return true;
+		}
+		else {
 		return false;
+	}
 	}
 	
 	public boolean checkEmpty() {
+		
 		return false;
 	}
 	
 	public void removeCard() {
+		
 		
 	}
 	public void addCard() {
@@ -32,3 +48,5 @@ public class Tableaus {
 	}
 
 }
+
+
