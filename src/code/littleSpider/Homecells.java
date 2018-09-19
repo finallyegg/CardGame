a0ce0c374af223164cef089ce1c7582cf161410a
kchen <kchen@LAPTOP-BGNH7HRT>
2018-09-19 15:00:10 -0400

    Homecells and tests for littlespider Added a size to homecells and finished some testing.

--- src/code/littleSpider/Homecells.java
+++ src/code/littleSpider/Homecells.java
@@ -5,20 +5,21 @@ import code.Deck.Card;
 
 public class Homecells {
 	
-	private Card card = new Card(null, null);
+	Card card = new Card(null, null);
 	private ArrayList<Card> cardStack;
 	
 	public Homecells(Card s) {
+		cardStack = new ArrayList<Card>();
 		card = s;
 	
-		cardStack.add(s);
+		cardStack.add(card);
 		
 	}
 	
 	//this gets the first card and checks to see if the suits match
-	public boolean checkSuit() {
+	public boolean checkSuit(Card c) {
 		
-		if(cardStack.get(0).getSuit() == card.getSuit() ) {
+		if(cardStack.get(0).getSuit() == c.getSuit() ) {
 			return true;
 		}
 		else {
@@ -28,12 +29,12 @@ public class Homecells {
 	}
 	//this checks to see if the first card is either 1 or -1 off of the original card. for ex if the index was King it would check if the new card's
 	//value is either a queen or a Ace and if it is it will return true
-	public boolean checkNumber() {
-		if(cardStack.get(0).value() + 1 == card.value() || cardStack.get(0).value() -1 == card.value()) {
+	public boolean checkNumber(Card c) {
+		if((cardStack.get(0).value() + 1) == c.value() || cardStack.get(0).value() -1 == c.value()) {
 			return true;
 		}
 		else {
-		return false;
+			return false;
 		}
 	}
 	
@@ -42,7 +43,11 @@ public class Homecells {
 	}
 	
 	public void removeCard() {
-	
+		
+	}
+
+	public int size() {
+		return cardStack.size();
 	}
 
 }
--- src/tests/littleSpiderTests/HomecellsTest.java
+++ src/tests/littleSpiderTests/HomecellsTest.java
@@ -4,26 +4,66 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class HomecellsTest {
+import code.Deck.Card;
+import code.Deck.Card.Ranks;
+import code.Deck.Card.Suits;
+import code.littleSpider.Homecells;
 
+public class HomecellsTest {
+
+	Card c1 = new Card(Ranks.ACE, Suits.HEARTS);
+	Card c2 = new Card(Ranks.TWO, Suits.HEARTS);
+	Card c3 = new Card(Ranks.KING, Suits.DIAMONDS);
+	Card c4 = new Card(Ranks.FIVE, Suits.SPADES);
+	Card c5 = new Card(Ranks.JACK, Suits.CLUBS);
+	Card c6 = new Card(Ranks.ACE, Suits.SPADES);
+	Card c7 = new Card(Ranks.ACE, Suits.CLUBS);
+	Card c8 = new Card(Ranks.KING, Suits.DIAMONDS);
+	Card c9 = new Card(Ranks.KING, Suits.HEARTS);
+	Card c10 = new Card(Ranks.ACE, Suits.DIAMONDS);
+	Card c11 = new Card(Ranks.KING, Suits.SPADES);
+	Card c12 = new Card(Ranks.KING, Suits.CLUBS);
+	
+	Homecells h = new Homecells(c1);
+	Homecells h2 = new Homecells(c10);
+	Homecells h3 = new Homecells(c11);
+	Homecells h4 = new Homecells(c12);
+	
 	@Test
-	void testCheckSuit() {
-		fail("Not yet implemented");
+	public void testInitialSetup() {
+		assertEquals(1, h.size());
+	}
+	
+	@Test
+	public void testCheckSuit() {
+		boolean b1 = h.checkSuit(c2);
+		boolean b2 = h.checkSuit(c3);
+		boolean b3 = h.checkSuit(c4);
+		boolean b4 = h.checkSuit(c5);
+		assertTrue(b1);
+		assertFalse(b2);
+		assertFalse(b3);
+		assertFalse(b4);
 	}
 
 	@Test
-	void testCheckNumber() {
-		fail("Not yet implemented");
+	public void testCheckNumber() {
+		boolean b1 = h.checkNumber(c2);
+		boolean b2 = h.checkNumber(c3);
+		boolean b3 = h.checkNumber(c7);
+		assertTrue(b1);
+		assertFalse(b2);
+		assertFalse(b3);
 	}
 
 	@Test
-	void testCheckFinished() {
-		fail("Not yet implemented");
+	public void testCheckFinished() {
+		
 	}
 
 	@Test
-	void testRemoveCard() {
-		fail("Not yet implemented");
+	public void testRemoveCard() {
+		
 	}
 
 }
