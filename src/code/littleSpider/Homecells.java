3acff668db149d4438bf2f9ccf9cae87fa091daa
kchen <kchen@LAPTOP-BGNH7HRT>
2018-09-19 17:19:00 -0400

    Homecells Test Update All tests currently pass but not sure if logic is completely correct.

--- src/code/littleSpider/Homecells.java
+++ src/code/littleSpider/Homecells.java
@@ -2,6 +2,8 @@ package code.littleSpider;
 import java.util.ArrayList;
 
 import code.Deck.Card;
+import code.Deck.Card.Ranks;
+import code.Deck.Card.Suits;
 
 public class Homecells {
 	
@@ -30,7 +32,14 @@ public class Homecells {
 	//this checks to see if the first card is either 1 or -1 off of the original card. for ex if the index was King it would check if the new card's
 	//value is either a queen or a Ace and if it is it will return true
 	public boolean checkNumber(Card c) {
-		if((cardStack.get(0).value() + 1) == c.value() || cardStack.get(0).value() -1 == c.value()) {
+		if(cardStack.get(0).value() == 14) {
+			if(c.value() == 2) {
+				return true;
+			}
+			else
+				return false;
+		}
+		else if((cardStack.get(0).value() + 1) == c.value() || cardStack.get(0).value() -1 == c.value()) {
 			return true;
 		}
 		else {
@@ -49,6 +58,14 @@ public class Homecells {
 	public int size() {
 		return cardStack.size();
 	}
-
+	
+	public static void main(String[] args) {
+		Card c = new Card(Ranks.ACE, Suits.HEARTS);
+		Card c2 = new Card(Ranks.TWO, Suits.HEARTS);
+		Card c3 = new Card(Ranks.KING, Suits.CLUBS);
+		Homecells h = new Homecells(c);
+		System.out.println(h.checkNumber(c2));
+		System.out.println(h.checkNumber(c3));
+	}
 }
 
