8397aa1b2e5dc73558230616467f9a7e94fb1732
kchen <kchen@LAPTOP-BGNH7HRT>
2018-09-28 18:33:54 -0400

    Cleaned up the littleSpiderTests and updated test.txt Just deleted all unnecessary code from littleSpiderTests and uploaded all test data on HomecellsTest

--- src/tests/littleSpiderTests/LittleSpiderTest.java
+++ src/tests/littleSpiderTests/LittleSpiderTest.java
@@ -8,32 +8,7 @@ class LittleSpiderTest {
 
 	@Test
 	void testLittleSpider() {
-		fail("Not yet implemented");
-	}
 
-	@Test
-	void testDealGame() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testMoveCard() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testMoveToHomecell() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testCheckWin() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testRestart() {
-		fail("Not yet implemented");
 	}
 
 }
