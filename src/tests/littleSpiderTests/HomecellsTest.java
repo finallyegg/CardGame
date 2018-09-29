8ff459293b7d2bb6e1dc28ac2cb64d331d7d8f0e
kchen <kchen@LAPTOP-BGNH7HRT.fios-router.home>
2018-09-28 22:03:09 -0400

    Fixed tests to fit rubric criteria

--- src/tests/littleSpiderTests/HomecellsTest.java
+++ src/tests/littleSpiderTests/HomecellsTest.java
@@ -42,15 +42,12 @@ public class HomecellsTest {
 	}
 	
 	@Test
-	public void testCheckSuit() {
+	public void testLegalToAdd() {
 		assertTrue("Should accept the correct suit (Hearts).", h.checkSuit(c2));
 		assertFalse("Should reject the wrong suit (Diamonds)", h.checkSuit(c3));
 		assertFalse("Should reject the wrong suit (Spades)", h.checkSuit(c4));
 		assertFalse("Should reject the wrong suit (Clubs)", h.checkSuit(c5));
-	}
-
-	@Test
-	public void testCheckNumber() {
+		
 		Homecells h10 = new Homecells(c1);
 		Homecells h11 = new Homecells(c11);
 		Card two = new Card(Ranks.TWO, Suits.SPADES);
@@ -70,27 +67,6 @@ public class HomecellsTest {
 	}
 
 	@Test
-	public void testCheckFinished() {
-		h.alwaysAddCard(c9);
-		h4.alwaysAddCard(c7);
-		assertTrue("Should return true if game starts with an Ace of 'Suit' and ends with King of same 'Suit'", h.checkFinished());
-		assertTrue("Should return true if game starts with an King of 'Suit' and ends with Ace of same 'Suit'", h4.checkFinished());
-		assertFalse("Testing that it correctly identifies an incomplete homecell", h2.checkFinished());
-	}
-
-	@Test
-	public void testRemoveCard() {
-		Homecells h5 = new Homecells(c1);
-		h5.removeCard();
-		assertEquals("First card should stay", 1, h5.size());
-		h5.addCard(c2);
-		Card c = h5.getCard(0);
-		h5.removeCard();
-		assertEquals("Size should decrease by one", 1, h5.size());
-		assertEquals("The previous card should now be the first one", c, h5.getCard(0));
-	}
-	
-	@Test
 	public void testAddCard() {
 		Homecells h7 = new Homecells(c1);
 		Card c = new Card(Ranks.TWO, Suits.DIAMONDS);
@@ -101,25 +77,34 @@ public class HomecellsTest {
 		assertTrue("Testing a correct card", h7.addCard(cq));
 		assertEquals("The top card should now be the newly added one", cq, h7.getCard(h7.size() - 1));
 		assertEquals("Homecell should increase in size by 1", 2, h7.size());
+	/*
+		h.alwaysAddCard(c9);
+		h4.alwaysAddCard(c7);
+		assertTrue("Should return true if game starts with an Ace of 'Suit' and ends with King of same 'Suit'", h.checkFinished());
+		assertTrue("Should return true if game starts with an King of 'Suit' and ends with Ace of same 'Suit'", h4.checkFinished());
+		assertFalse("Testing that it correctly identifies an incomplete homecell", h2.checkFinished());
+	*/
+	}
+
+	@Test
+	public void testRemoveCard() {
+		Homecells h5 = new Homecells(c1);
+		h5.removeCard();
+		assertEquals("First card should stay", 1, h5.size());
+		h5.addCard(c2);
+		Card c = h5.getCard(0);
+		h5.removeCard();
+		assertEquals("Size should equal 1", 1, h5.size());
 	}
 	
 	@Test
-	public void testCheckMoveToTableau() {
-		Deck qwe = new Deck();
-		Card ca = new Card(Ranks.SIX, Suits.DIAMONDS);
-		Card cb = new Card(Ranks.SEVEN, Suits.DIAMONDS);
-		Card cc = new Card(Ranks.SIX, Suits.CLUBS);
-		ArrayList<Card> a = new ArrayList<Card>();
-		ArrayList<Card> a1 = new ArrayList<Card>();
-		Homecells h6 = new Homecells(ca);
-		Tableaus t = new Tableaus(a,qwe);
-		Tableaus t1 = new Tableaus(a1,qwe);
-		a.add(0, ca);
-		a1.add(0, cc);
-		assertFalse("Testing that the bottom card can't be removed", h6.checkMoveToTableau(t));
-		h6.alwaysAddCard(cb);
-		assertTrue("Testing that the card can move if valid (a lower number and same suit)", h6.checkMoveToTableau(t));
-		assertTrue("Testing that the card can move if valid (a higher number and different suit)", h6.checkMoveToTableau(t1));
+	public void testHomecellRemove() {
+		Homecells h5 = new Homecells(c1);
+		h5.addCard(c2);
+		Card c = h5.getCard(0);
+		h5.removeCard();
+		assertEquals("Size should decrease by one", 1, h5.size());
+		assertEquals("The previous card should now be the first one", c, h5.getCard(0));
 	}
 
 }
