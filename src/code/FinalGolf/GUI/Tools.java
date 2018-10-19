2f27ba7ce75b6180f78b10738a14013c132a90f2
Zeping <Zeping(zepinghe@buffalo.edu)>
2018-10-19 16:19:07 -0400

    add icon pack

--- src/code/FinalGolf/GUI/Tools.java
+++ src/code/FinalGolf/GUI/Tools.java
@@ -1,13 +1,14 @@
 package code.FinalGolf.GUI;
 
 import java.awt.Dimension;
-
+import java.util.HashMap;
 import javax.swing.*;
+import code.Deck.Card;
 
 public class Tools {
-	private JLabel createDisplayImage(String fileNameRelativeToClassFile) {
+	public static JLabel createDisplayImage(String fileNameRelativeToClassFile) {
 	    JLabel retVal = new JLabel();
-	    java.net.URL imgURL = this.getClass().getResource(fileNameRelativeToClassFile);
+	    java.net.URL imgURL = Tools.class.getResource(fileNameRelativeToClassFile);
 	    if (imgURL == null) {
 	      throw new IllegalArgumentException("Couldn't find file: " + fileNameRelativeToClassFile);
 	    }
@@ -20,4 +21,37 @@ public class Tools {
 	    retVal.setMinimumSize(d);
 	    return retVal;
 	  }
+	
+	public static JLabel getCardLabel (Card x) {
+		HashMap<Card,String> cardNameMap = new HashMap<>();
+
+		for (Card.Suits i : Card.Suits.values()) {
+			cardNameMap.put(new Card(Card.Ranks.ACE,i), "a"+i.toString().toLowerCase().split("")[0]);
+			cardNameMap.put(new Card(Card.Ranks.TWO,i), "2"+i.toString().toLowerCase().split("")[0]);
+			cardNameMap.put(new Card(Card.Ranks.THREE,i), "3"+i.toString().toLowerCase().split("")[0]);
+			cardNameMap.put(new Card(Card.Ranks.FOUR,i), "4"+i.toString().toLowerCase().split("")[0]);
+			cardNameMap.put(new Card(Card.Ranks.FIVE,i), "5"+i.toString().toLowerCase().split("")[0]);
+			cardNameMap.put(new Card(Card.Ranks.SIX,i), "6"+i.toString().toLowerCase().split("")[0]);
+			cardNameMap.put(new Card(Card.Ranks.SEVEN,i), "7"+i.toString().toLowerCase().split("")[0]);
+			cardNameMap.put(new Card(Card.Ranks.EIGHT,i), "8"+i.toString().toLowerCase().split("")[0]);
+			cardNameMap.put(new Card(Card.Ranks.NINE,i), "9"+i.toString().toLowerCase().split("")[0]);
+			cardNameMap.put(new Card(Card.Ranks.TEN,i), "10"+i.toString().toLowerCase().split("")[0]);
+			cardNameMap.put(new Card(Card.Ranks.JACK,i), "j"+i.toString().toLowerCase().split("")[0]);
+			cardNameMap.put(new Card(Card.Ranks.QUEEN,i), "q"+i.toString().toLowerCase().split("")[0]);
+			cardNameMap.put(new Card(Card.Ranks.KING,i), "k"+i.toString().toLowerCase().split("")[0]);
+		}
+		for (Card i :cardNameMap.keySet()) {
+			if(i.getRank().equals(x.getRank())&&i.getSuit().equals(x.getSuit())){
+				x=i;
+			}
+		}
+//		System.out.print(cardNameMap);
+		String filename = cardNameMap.get(x);
+//		Tools.createDisplayImage("/S18SemesterProject/src/IconPack/" + filename + ".gif");
+		return Tools.createDisplayImage("/src/IconPack/" + filename + ".gif");
+	}
+	
+	public static void main(String[] args) {
+		System.out.print(Tools.getCardLabel(new Card(Card.Ranks.TWO,Card.Suits.HEARTS)));
+	}
 }
