c5ab3e785827a228bba5050bffe3b896f9e188d5
Deanna <deyanafa@buffalo.edu>
2018-10-19 15:05:38 -0400

    mouselistener

--- src/code/FinalGolf/GUI/GolfMouseListener.java
+++ src/code/FinalGolf/GUI/GolfMouseListener.java
@@ -10,54 +10,54 @@ import code.FinalGolf.Golf_Homecells;
 import code.FinalGolf.Golf_Tableaus;
 
 public class GolfMouseListener implements MouseListener{
-	
+
 	private JButton B1;
 	private Card topcard;
 	private Golf_Tableaus golfTablue;
 	private Golf_Homecells golfHomeCell;
-	
+
 	public GolfMouseListener(JButton x, Golf_Tableaus b, Golf_Homecells c) {
 		B1 = x;
 		golfTablue = b;
 		golfHomeCell = c;
 		topcard = b.getTablestack().get(0);
 	}
-	
+
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
-		
-	if(golfHomeCell.getHomecellstack().add(topcard) == true) {
-		System.out.println("Card added to Homecell");
+
+		if(golfHomeCell.getHomecellstack().add(topcard) == true) {
+			System.out.println("Card added to Homecell");
+		}
+		else {
+			System.out.println("Not a legal move");
+		}
+
 	}
-	else {
-		System.out.println("Not a legal move");
-	}
-		
-	}
-		
-	
+
+
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
