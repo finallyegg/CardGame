0ba065af90444f3ae53f7a8e30e9b0cfce39b1a9
Deanna <deyanafa@buffalo.edu>
2018-10-19 14:58:57 -0400

    mouseEvent Clicked

--- src/code/FinalGolf/GUI/GolfMouseListener.java
+++ src/code/FinalGolf/GUI/GolfMouseListener.java
@@ -5,15 +5,44 @@ import java.awt.event.MouseListener;
 
 import javax.swing.*;
 
+import code.Deck.Card;
+import code.FinalGolf.Golf_Homecells;
+import code.FinalGolf.Golf_Tableaus;
+
 public class GolfMouseListener implements MouseListener{
 	
 	private JButton B1;
+	private Card topcard;
+	private Golf_Tableaus golfTablue;
+	private Golf_Homecells golfHomeCell;
+	
+	public GolfMouseListener(JButton x, Golf_Tableaus b, Golf_Homecells c) {
+		B1 = x;
+		golfTablue = b;
+		golfHomeCell = c;
+		topcard = b.getTablestack().get(0);
+	}
 	
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
+		
+	if(golfHomeCell.getHomecellstack().add(topcard) == true) {
+		System.out.println("Card added to Homecell");
+	}
+	else {
+		System.out.println("Not a legal move");
+	}
+	
+	
+	
 		
 	}
+		
+		
+		
+		
+		
+	
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
