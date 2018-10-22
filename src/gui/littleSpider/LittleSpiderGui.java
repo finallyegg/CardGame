d48839c851782cfaa2788c97e68e8fb4021388ad
yingwei123 <yli283@buffalo.edu>
2018-10-22 15:19:08 -0400

    qwe

--- src/gui/littleSpider/LittleSpiderGui.java
+++ src/gui/littleSpider/LittleSpiderGui.java
@@ -1,28 +1,46 @@
 package gui.littleSpider;
 
 import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.tools.Tool;
 
+import code.Deck.Card;
+import code.FinalGolf.GUI.Tools;
 import code.littleSpider.LittleSpider;
 
-public class LittleSpiderGui {
+public class LittleSpiderGui implements MouseListener {
 	private LittleSpider ls;
+	private JPanel tab;
+	private JPanel home;
 	private static JFrame game;
+	private Card checker = new Card(null,null);
+	private int tabPressed;
+	private JLayeredPane[] la = new JLayeredPane[8]; //for Tab cards
+
 	
-	public LittleSpiderGui() {
-		ls = new LittleSpider();
-		JFrame frame = new JFrame("Little Spider");
-	    frame.setSize(1000, 600);
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.setVisible(true);
-	    
 	
+	
+	public LittleSpiderGui(LittleSpider idk) {
+		ls = idk;
+		tab = new JPanel();
+		home = new JPanel();
+	
+		
+	   game.add(home);
+	   game.add(tab);
+		
+
 	}
+	
 	private JLabel createDisplayImage(String fileNameRelativeToClassFile) {
 	    JLabel retVal = new JLabel();
 	    java.net.URL imgURL = this.getClass().getResource(fileNameRelativeToClassFile);
@@ -37,32 +55,59 @@ public class LittleSpiderGui {
 	    retVal.setMaximumSize(d);
 	    retVal.setMinimumSize(d);
 	    
+	       
+	    
+	    
+	    
 	    
 	    return retVal;
 	    
 	    
 	  }
-	
-	
-	public static JFrame startGame() {
+	@Override
+	public void mouseClicked(MouseEvent e) {
 		
-		return game;
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+		
+	public void setTableaus() {
+		
+	}
+	
+	public JPanel getTableaus() {
+		return tab;
+		
+	}
+	
+	public void setHome() {
+		
+	}
+	public JPanel getHome() {
+		return home;
 	}
 	
 	
 	
-	  
-	
-	
-	public static void main(String[] args){
-	    SwingUtilities.invokeLater(new Runnable() {
-	        @Override
-	        public void run() {
-	        	LittleSpider qwe = new LittleSpider();
-	        	LittleSpiderGui game = new	LittleSpiderGui();
-	        	startGame();
-	        
-	        }
-	    });
 	}
-}
+	
+	
---
+++ src/gui/littleSpider/runGui.java
@@ -0,0 +1,36 @@
+package gui.littleSpider;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import code.littleSpider.LittleSpider;
+
+public class runGui {
+
+		public static void runGUI(){
+			LittleSpider ls = new LittleSpider();
+        	JFrame frame = new JFrame("Little Spider");
+        	LittleSpiderGui lol = new LittleSpiderGui(ls);
+    	    frame.setSize(1000, 600);
+    	    frame.getContentPane().add(lol.getHome(),  BorderLayout.SOUTH);
+    	    frame.getContentPane().add(lol.getTableaus(),  BorderLayout.NORTH);
+    	    
+    	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	    frame.pack();
+    	    frame.setVisible(true);
+    	   
+		}
+			public static void main(String[] args){
+	    SwingUtilities.invokeLater(new Runnable() {
+	        @Override
+	        public void run() {
+	        	
+	        	runGUI();
+	        
+	        }
+	    });
+	}
+}
+
